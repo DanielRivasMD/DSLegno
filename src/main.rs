d9268c470f5817de2cf5dfd2efb6e146b0f5291c
DanielRivasMD <danielrivasmd@gmail.com>
2024-09-25 06:44:13 +0200

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,204 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#![allow(non_snake_case)]
+use std::fs::File;
+use std::io::BufReader;
+use xml::common::Position;
+use xml::reader::{ParserConfig, XmlEvent};
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-use dioxus::prelude::*;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
-
+// TODO: parser functional. select desired fields. IMPORTANT: multiple records on one file
 fn main() {
-	launch(App);
-}
+    let file_path = std::env::args_os().nth(1).expect("Please specify a path to an XML file");
+    let file = File::open(file_path).unwrap();
 
-pub fn App() -> Element {
-	rsx! {
-		StoryListing {
-			story: StoryItem {
-				id: 0,
-				title: "hello hackernews".to_string(),
-				url: None,
-				text: None,
-				by: "Author".to_string(),
-				score: 0,
-				descendants: 0,
-				time: chrono::Utc::now(),
-				kids: vec![],
-				r#type: "".to_string(),
-			}
-		}
-	}
-}
+    let mut reader = ParserConfig::default()
+        .ignore_root_level_whitespace(false)
+        .create_reader(BufReader::new(file));
 
-// Define the Hackernews types
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct StoryPageData {
-	#[serde(flatten)]
-	pub item: StoryItem,
-	#[serde(default)]
-	pub comments: Vec<Comment>,
-}
+    loop {
+        match reader.next() {
+            Ok(e) => {
+                print!("{}\t", reader.position());
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct Comment {
-	pub id: i64,
-	/// there will be no by field if the comment was deleted
-	#[serde(default)]
-	pub by: String,
-	#[serde(default)]
-	pub text: String,
-	#[serde(with = "chrono::serde::ts_seconds")]
-	pub time: DateTime<Utc>,
-	#[serde(default)]
-	pub kids: Vec<i64>,
-	#[serde(default)]
-	pub sub_comments: Vec<Comment>,
-	pub r#type: String,
-}
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct StoryItem {
-	pub id: i64,
-	pub title: String,
-	pub url: Option<String>,
-	pub text: Option<String>,
-	#[serde(default)]
-	pub by: String,
-	#[serde(default)]
-	pub score: i64,
-	#[serde(default)]
-	pub descendants: i64,
-	#[serde(with = "chrono::serde::ts_seconds")]
-	pub time: DateTime<Utc>,
-	#[serde(default)]
-	pub kids: Vec<i64>,
-	pub r#type: String,
-}
-
-#[component]
-fn StoryListing(story: ReadOnlySignal<StoryItem>) -> Element {
-	let StoryItem {
-		title,
-		url,
-		by,
-		score,
-		time,
-		kids,
-		..
-	} = &*story.read();
-
-	let url = url.as_deref().unwrap_or_default();
-	let hostname = url
-		.trim_start_matches("https://")
-		.trim_start_matches("http://")
-		.trim_start_matches("www.");
-	let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
-	let comments = format!(
-		"{} {}",
-		kids.len(),
-		if kids.len() == 1 {
-			" comment"
-		} else {
-			" comments"
-		}
-	);
-	let time = time.format("%D %l:%M %p");
-
-	rsx! {
-		div { padding: "0.5rem", position: "relative",
-			div { font_size: "1.5rem",
-				a { href: url, "{title}" }
-				a {
-					color: "gray",
-					href: "https://news.ycombinator.com/from?site={hostname}",
-					text_decoration: "none",
-					" ({hostname})"
-				}
-			}
-			div { display: "flex", flex_direction: "row", color: "gray",
-				div { "{score}" }
-				div { padding_left: "0.5rem", "by {by}" }
-				div { padding_left: "0.5rem", "{time}" }
-				div { padding_left: "0.5rem", "{comments}" }
-            }
+                match e {
+                    XmlEvent::StartDocument { version, encoding, .. } => {
+                        println!("StartDocument({version}, {encoding})");
+                    },
+                    XmlEvent::EndDocument => {
+                        println!("EndDocument");
+                        break;
+                    },
+                    XmlEvent::ProcessingInstruction { name, data } => {
+                        println!("ProcessingInstruction({name}={:?})", data.as_deref().unwrap_or_default());
+                    },
+                    XmlEvent::StartElement { name, attributes, .. } => {
+                        if attributes.is_empty() {
+                            println!("StartElement({name})");
+                        } else {
+                            let attrs: Vec<_> = attributes
+                                .iter()
+                                .map(|a| format!("{}={:?}", &a.name, a.value))
+                                .collect();
+                            println!("StartElement({name} [{}])", attrs.join(", "));
+                        }
+                    },
+                    XmlEvent::EndElement { name } => {
+                        println!("EndElement({name})");
+                    },
+                    XmlEvent::Comment(data) => {
+                        println!(r#"Comment("{}")"#, data.escape_debug());
+                    },
+                    XmlEvent::CData(data) => println!(r#"CData("{}")"#, data.escape_debug()),
+                    XmlEvent::Characters(data) => {
+                        println!(r#"Characters("{}")"#, data.escape_debug());
+                    },
+                    XmlEvent::Whitespace(data) => {
+                        println!(r#"Whitespace("{}")"#, data.escape_debug());
+                    },
+                }
+            },
+            Err(e) => {
+                eprintln!("Error at {}: {e}", reader.position());
+                break;
+            },
         }
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// #![allow(non_snake_case)]
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// use dioxus::prelude::*;
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// use chrono::{DateTime, Utc};
+// use serde::{Deserialize, Serialize};
+
+// fn main() {
+// 	launch(App);
+// }
+
+// pub fn App() -> Element {
+// 	rsx! {
+// 		StoryListing {
+// 			story: StoryItem {
+// 				id: 0,
+// 				title: "hello hackernews".to_string(),
+// 				url: None,
+// 				text: None,
+// 				by: "Author".to_string(),
+// 				score: 0,
+// 				descendants: 0,
+// 				time: chrono::Utc::now(),
+// 				kids: vec![],
+// 				r#type: "".to_string(),
+// 			}
+// 		}
+// 	}
+// }
+
+// // Define the Hackernews types
+// #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+// pub struct StoryPageData {
+// 	#[serde(flatten)]
+// 	pub item: StoryItem,
+// 	#[serde(default)]
+// 	pub comments: Vec<Comment>,
+// }
+
+// #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+// pub struct Comment {
+// 	pub id: i64,
+// 	/// there will be no by field if the comment was deleted
+// 	#[serde(default)]
+// 	pub by: String,
+// 	#[serde(default)]
+// 	pub text: String,
+// 	#[serde(with = "chrono::serde::ts_seconds")]
+// 	pub time: DateTime<Utc>,
+// 	#[serde(default)]
+// 	pub kids: Vec<i64>,
+// 	#[serde(default)]
+// 	pub sub_comments: Vec<Comment>,
+// 	pub r#type: String,
+// }
+
+// #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+// pub struct StoryItem {
+// 	pub id: i64,
+// 	pub title: String,
+// 	pub url: Option<String>,
+// 	pub text: Option<String>,
+// 	#[serde(default)]
+// 	pub by: String,
+// 	#[serde(default)]
+// 	pub score: i64,
+// 	#[serde(default)]
+// 	pub descendants: i64,
+// 	#[serde(with = "chrono::serde::ts_seconds")]
+// 	pub time: DateTime<Utc>,
+// 	#[serde(default)]
+// 	pub kids: Vec<i64>,
+// 	pub r#type: String,
+// }
+
+// #[component]
+// fn StoryListing(story: ReadOnlySignal<StoryItem>) -> Element {
+// 	let StoryItem {
+// 		title,
+// 		url,
+// 		by,
+// 		score,
+// 		time,
+// 		kids,
+// 		..
+// 	} = &*story.read();
+
+// 	let url = url.as_deref().unwrap_or_default();
+// 	let hostname = url
+// 		.trim_start_matches("https://")
+// 		.trim_start_matches("http://")
+// 		.trim_start_matches("www.");
+// 	let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
+// 	let comments = format!(
+// 		"{} {}",
+// 		kids.len(),
+// 		if kids.len() == 1 {
+// 			" comment"
+// 		} else {
+// 			" comments"
+// 		}
+// 	);
+// 	let time = time.format("%D %l:%M %p");
+
+// 	rsx! {
+// 		div { padding: "0.5rem", position: "relative",
+// 			div { font_size: "1.5rem",
+// 				a { href: url, "{title}" }
+// 				a {
+// 					color: "gray",
+// 					href: "https://news.ycombinator.com/from?site={hostname}",
+// 					text_decoration: "none",
+// 					" ({hostname})"
+// 				}
+// 			}
+// 			div { display: "flex", flex_direction: "row", color: "gray",
+// 				div { "{score}" }
+// 				div { padding_left: "0.5rem", "by {by}" }
+// 				div { padding_left: "0.5rem", "{time}" }
+// 				div { padding_left: "0.5rem", "{comments}" }
+//             }
+//         }
+//     }
+// }
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////////
