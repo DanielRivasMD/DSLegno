0e08153f9c4f45ecc4e54bd5a943ed0dcb2dcc4d
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-09 15:55:17 +0200

    add custom root

---
+++ src/custom.rs
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// modules
+pub mod fattura_header;
+pub mod participant;
+pub mod dati_anagrafica;
+pub mod id_fiscale;
+pub mod anagrafica;
+pub mod sede;
+pub mod iscrizione_rea;
+
+pub mod fattura_body;
+pub mod dati_generale;
+pub mod dati_beni_servizi;
+pub mod dettaglio_linee;
+pub mod dati_riepilogo;
+pub mod dati_pagamento;
+pub mod dettaglio_pagamento;
+
+pub mod tag;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// struct DatiTrasmittente {
+
+// 	id_trasmittente: IDTrasmittente,
+
+// }
+
+// struct IDTrasmittente {
+
+// 	id_paese: String,
+
+// 	id_codice: String,
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
