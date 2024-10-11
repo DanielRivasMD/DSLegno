76f5d72a5366ab915a228d50fb4ce945ee719532
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-11 07:03:31 +0200

    add hard coded get db path

--- src/utils/xml_parser.rs
+++ src/utils/xml_parser.rs
@@ -9,6 +9,14 @@ use xml::reader::{ParserConfig, XmlEvent};
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn xml_parser(reader: EventReader<BufReader<File>>) {
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn get_db_path() -> String {
+	// let home_dir = dirs::home_dir().unwrap();
+	// home_dir.to_str().unwrap().to_string() + "/.config/orion/database.sqlite"
+	"dallasanta.sql".to_string()
+}
+
 	loop {
 		match reader.next() {
 			Ok(e) => {
