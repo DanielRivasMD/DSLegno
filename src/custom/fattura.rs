54f425396f7d8f84d6318d3fa126e105e07e00e7
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 11:46:31 +0200

    add fattura to upload struct custom

--- src/custom/fattura.rs
+++ src/custom/fattura.rs
@@ -22,6 +22,65 @@ pub mod dettaglio_pagamento;
 // crate utilities
 use fattura_header::*;
 use fattura_body::*;
+// TODO: find how to transfer fields
+#[derive(Debug, Default, Deserialize, Insertable, Serialize, new)]
+#[diesel(table_name = tabella_vendite)]
+pub struct FatturaToUpload {
+
+	#[new(default)]
+	pub descrizione: String,
+
+	#[new(default)]
+	pub quantita: String,
+
+	#[new(default)]
+	pub prezzo_unitario: String,
+
+	#[new(default)]
+	pub prezzo_totale: String,
+
+	#[new(default)]
+	pub aliquota_iva: String,
+
+	#[new(default)]
+	pub numero_fattura: String,
+
+	#[new(default)]
+	pub giorno_data: String,
+
+	#[new(default)]
+	pub importo_totale: String,
+
+	#[new(default)]
+	pub prestatore_denominazione: String,
+
+	#[new(default)]
+	pub prestatore_indirizzo: String,
+
+	#[new(default)]
+	pub committente_denominazione: String,
+
+	#[new(default)]
+	pub committente_indirizzo: String,
+
+	#[new(default)]
+	pub imponibile_importo: String,
+
+	#[new(default)]
+	pub imposta: String,
+
+	#[new(default)]
+	pub esigibilita_iva: String,
+
+	#[new(default)]
+	pub data_riferimento_termini: String,
+
+	#[new(default)]
+	pub data_scadenza_pagamento: String,
+
+	#[new(default)]
+	pub importo_pagamento: String,
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
