adf5f54f7804a1321a444d8c3417b81595a08221
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 11:48:06 +0200

    implement upload formatter fattura custom

--- src/custom/fattura.rs
+++ src/custom/fattura.rs
@@ -135,3 +135,36 @@ pub struct FatturaToCapture {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+impl FatturaToCapture {
+	pub fn upload_formatter(
+		&self,
+	) -> Vec<FatturaToUpload> {
+		let mut fatture: Vec<FatturaToUpload> = vec![];
+		for details in self.dettaglio_linee.iter() {
+			fatture.push(
+				FatturaToUpload {
+					descrizione: details.descrizione.clone(),
+					quantita: details.quantita.clone(),
+					prezzo_unitario: details.prezzo_unitario.clone(),
+					prezzo_totale: details.prezzo_totale.clone(),
+					aliquota_iva: details.aliquota_iva.clone(),
+					numero_fattura: self.numero_fattura.clone(),
+					giorno_data: self.giorno_data.clone(),
+					importo_totale: self.importo_totale.clone(),
+					prestatore_denominazione: self.prestatore_denominazione.clone(),
+					prestatore_indirizzo: self.prestatore_indirizzo.clone(),
+					committente_denominazione: self.committente_denominazione.clone(),
+					committente_indirizzo: self.committente_indirizzo.clone(),
+					imponibile_importo: self.imponibile_importo.clone(),
+					imposta: self.imposta.clone(),
+					esigibilita_iva: self.esigibilita_iva.clone(),
+					data_riferimento_termini: self.data_riferimento_termini.clone(),
+					data_scadenza_pagamento: self.data_scadenza_pagamento.clone(),
+					importo_pagamento: self.importo_pagamento.clone(),
+				}
+			);
+		}
+		return fatture;
+	}
+}
+
