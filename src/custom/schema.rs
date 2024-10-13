a26dbc91f9f5ab5a3abb9ee6bb7888844dcafce6
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 18:39:53 +0200

    add schema tabella acquisti

--- src/custom/schema.rs
+++ src/custom/schema.rs
@@ -1,5 +1,29 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+diesel::table! {
+	tabella_acquisti {
+		id -> Integer,
+		operazione -> Integer,
+		prestatore_denominazione -> Text,
+		prestatore_indirizzo -> Text,
+		committente_denominazione -> Text,
+		committente_indirizzo -> Text,
+		numero_fattura -> Text,
+		giorno_data -> Text,
+		importo_totale -> Text,
+		descrizione -> Text,
+		quantita -> Text,
+		prezzo_unitario -> Text,
+		prezzo_totale -> Text,
+		aliquota_iva -> Text,
+		imponibile_importo -> Text,
+		imposta -> Text,
+		esigibilita_iva -> Char,
+		data_riferimento_termini -> Text,
+		data_scadenza_pagamento -> Text,
+		importo_pagamento -> Text,
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
