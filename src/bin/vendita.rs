207d0c0d632f51f2673e52ac7b081d0d8b948833
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 18:39:37 +0200

    update vendita bin logic

--- src/bin/vendita.rs
+++ src/bin/vendita.rs
@@ -11,15 +11,29 @@ use std::fs::File;
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // crate utilities
+use crate::utils::sql::*;
 use crate::utils::xml_parser::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
+	// get path
 	let file_path = std::env::args_os().nth(1).expect("Please specify a path to an XML file");
 	let file = File::open(file_path).unwrap();
 
-	xml_parser(file);
+	// parse file
+	let fattura_to_capture = xml_parser(file);
+
+	// open database connection
+	let mut conn = establish_db_connection();
+
+	// prepare data to upload
+	let fatture_to_upload = fattura_to_capture.upload_formatter();
+
+	// upload to database
+	for fattura_to_upload in fatture_to_upload.into_iter() {
+		let _ = insert_insertable_struct(fattura_to_upload, &mut conn);
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
