e4bc986c47608323ae42f2a87b2a753155b672dd
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 11:50:18 +0200

    deprecate segregated structs custom

--- src/custom/fattura.rs
+++ src/custom/fattura.rs
@@ -1,27 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// modules
-pub mod fattura_header;
-pub mod participant;
-pub mod dati_anagrafica;
-pub mod id_fiscale;
-pub mod anagrafica;
-pub mod sede;
-pub mod iscrizione_rea;
-
-pub mod fattura_body;
-pub mod dati_generale;
-pub mod dati_beni_servizi;
-pub mod dettaglio_linee;
-pub mod dati_riepilogo;
-pub mod dati_pagamento;
-pub mod dettaglio_pagamento;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// crate utilities
-use fattura_header::*;
-use fattura_body::*;
 // TODO: find how to transfer fields
 #[derive(Debug, Default, Deserialize, Insertable, Serialize, new)]
 #[diesel(table_name = tabella_vendite)]
@@ -85,15 +66,12 @@ pub struct FatturaToUpload {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Default, new)]
-pub struct Fattura {
 pub struct FatturaToCapture {
 
 	#[new(default)]
-	pub fattura_header: FatturaHeader,
 	pub dettaglio_linee: Vec<DettaglioLinee>,
 
 	#[new(default)]
-	pub fattura_body: FatturaBody,
 	pub numero_fattura: String,
 
 	#[new(default)]
