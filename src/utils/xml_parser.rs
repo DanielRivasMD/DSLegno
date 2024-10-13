33d885c2b2308d50d95afbe301935dc3807cee7b
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-13 12:08:17 +0200

    update & cleanup xml parser

--- src/utils/xml_parser.rs
+++ src/utils/xml_parser.rs
@@ -15,35 +15,7 @@ use std::error::Error;
 // crate utilities
 use crate::custom::tag::*;
 use crate::custom::fattura::*;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-mod schema {
-	diesel::table! {
-		tabella_vendite {
-			id -> Integer,
-			operazione -> Integer,
-			prestatore_denominazione -> Text,
-			prestatore_indirizzo -> Text,
-			committente_denominazione -> Text,
-			committente_indirizzo -> Text,
-			fattura -> Text,
-			giorno_data -> Text,
-			importo_totale -> Text,
-			descrizione -> Text,
-			quantita -> Text,
-			prezzo_unitario -> Text,
-			prezzo_totale -> Text,
-			aliquota_iva -> Text,
-			imponibile_importo -> Text,
-			imposta -> Text,
-			esigibilita_iva -> Char,
-			data_riferimento_termini -> Text,
-			data_scadenza_pagamento -> Text,
-			importo_pagamento -> Text,
-		}
-	}
-}
+use crate::custom::schema::tabella_vendite::dsl::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
