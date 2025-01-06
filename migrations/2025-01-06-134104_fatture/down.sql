bbeb76c0b0101402d34c29692f8cb3b445ac380a
DanielRivasMD <danielrivasmd@gmail.com>
2025-01-06 07:46:12 -0600

    update migration down

--- migrations/2025-01-06-134104_fatture/down.sql
+++ migrations/2025-01-06-134104_fatture/down.sql
@@ -1,1 +1,6 @@
--- This file should undo anything in `up.sql`
+DROP TABLE tabella_principale;
+DROP TABLE tabella_acquisti;
+DROP TABLE tabella_vendite;
+DROP TABLE tabella_sommario;
+DROP TABLE tabella_mensile;
+DROP TABLE tabella_annualmente;
