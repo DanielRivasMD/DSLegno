bde8260de22a605b6da1f1dc71201ac3c882a40c
DanielRivasMD <danielrivasmd@gmail.com>
2025-01-07 22:13:57 -0600

    hard patch fattura custom

--- src/custom/fattura.rs
+++ src/custom/fattura.rs
@@ -11,10 +11,69 @@ use crate::custom::schema::*;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// TODO: find how to transfer fields
+// TODO: find how to transfer fields & use generics
+#[derive(Debug, Default, Insertable, new)]
+#[diesel(table_name = tabella_acquisti)]
+pub struct FatturaToUploadAcquisti {
+
+	#[new(default)]
+	pub descrizione: String,
+
+	#[new(default)]
+	pub quantita: String,
+
+	#[new(default)]
+	pub prezzo_unitario: String,
+
+	#[new(default)]
+	pub prezzo_totale: String,
+
+	#[new(default)]
+	pub aliquota_iva: String,
+
+	#[new(default)]
+	pub numero_fattura: String,
+
+	#[new(default)]
+	pub giorno_data: String,
+
+	#[new(default)]
+	pub importo_totale: String,
+
+	#[new(default)]
+	pub prestatore_denominazione: String,
+
+	#[new(default)]
+	pub prestatore_indirizzo: String,
+
+	#[new(default)]
+	pub committente_denominazione: String,
+
+	#[new(default)]
+	pub committente_indirizzo: String,
+
+	#[new(default)]
+	pub imponibile_importo: String,
+
+	#[new(default)]
+	pub imposta: String,
+
+	#[new(default)]
+	pub esigibilita_iva: String,
+
+	#[new(default)]
+	pub data_riferimento_termini: String,
+
+	#[new(default)]
+	pub data_scadenza_pagamento: String,
+
+	#[new(default)]
+	pub importo_pagamento: String,
+}
+
 #[derive(Debug, Default, Insertable, new)]
 #[diesel(table_name = tabella_vendite)]
-pub struct FatturaToUpload {
+pub struct FatturaToUploadVendite {
 
 	#[new(default)]
 	pub descrizione: String,
@@ -121,18 +180,55 @@ pub struct FatturaToCapture {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: use generic implementation
 impl FatturaToCapture {
-	pub fn upload_formatter(
+	pub fn upload_formatter_acquisti(
 		&self,
-	) -> anyResult<Vec<FatturaToUpload>> {
+	) -> anyResult<Vec<FatturaToUploadAcquisti>> {
 
 		// preallocate
-		let mut fatture: Vec<FatturaToUpload> = vec![];
+		let mut fatture: Vec<FatturaToUploadAcquisti> = vec![];
 
 		// iterate on details
 		for details in self.dettaglio_linee.iter() {
 			fatture.push(
-				FatturaToUpload {
+				FatturaToUploadAcquisti {
+					descrizione: details.descrizione.clone(),
+					quantita: details.quantita.clone(),
+					prezzo_unitario: details.prezzo_unitario.clone(),
+					prezzo_totale: details.prezzo_totale.clone(),
+					aliquota_iva: details.aliquota_iva.clone(),
+					numero_fattura: self.numero_fattura.clone(),
+					giorno_data: self.giorno_data.clone(),
+					importo_totale: self.importo_totale.clone(),
+					prestatore_denominazione: self.prestatore_denominazione.clone(),
+					prestatore_indirizzo: self.prestatore_indirizzo.clone(),
+					committente_denominazione: self.committente_denominazione.clone(),
+					committente_indirizzo: self.committente_indirizzo.clone(),
+					imponibile_importo: self.imponibile_importo.clone(),
+					imposta: self.imposta.clone(),
+					esigibilita_iva: self.esigibilita_iva.clone(),
+					data_riferimento_termini: self.data_riferimento_termini.clone(),
+					data_scadenza_pagamento: self.data_scadenza_pagamento.clone(),
+					importo_pagamento: self.importo_pagamento.clone(),
+				}
+			);
+		}
+
+		Ok(fatture)
+	}
+
+	pub fn upload_formatter_vendite(
+		&self,
+	) -> anyResult<Vec<FatturaToUploadVendite>> {
+
+		// preallocate
+		let mut fatture: Vec<FatturaToUploadVendite> = vec![];
+
+		// iterate on details
+		for details in self.dettaglio_linee.iter() {
+			fatture.push(
+				FatturaToUploadVendite {
 					descrizione: details.descrizione.clone(),
 					quantita: details.quantita.clone(),
 					prezzo_unitario: details.prezzo_unitario.clone(),
