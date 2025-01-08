35a9e3f82dd756886485b7106de1c99d2388906f
DanielRivasMD <danielrivasmd@gmail.com>
2025-01-07 22:14:05 -0600

    add fattura bin

---
+++ src/bin/fattura.rs
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// library wrapper
+use ds_legno::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// standard libraries
+use anyhow::Result as anyResult;
+use clap::Parser;
+use std::fs::File;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// crate utilities
+// use crate::custom::log_flag::LogFlag;
+use crate::custom::invoice::InvoiceType;
+use crate::utils::help::*;
+use crate::utils::sql::*;
+use crate::utils::xml_parser::*;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// DOC: first argument will be used as input
+// DOC: check whether invoice number exists prior to inserting
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn main() -> anyResult<()> {
+  // collect command line arguments
+  let params = Cli::parse();
+
+	// get path
+	let file_path = params.input;
+	let file = File::open(file_path)?;
+
+	// parse file
+	let fattura_to_capture = xml_parser(file)?;
+
+	// open database connection
+	let mut conn = establish_db_connection()?;
+
+	match params.invoice {
+		
+		InvoiceType::Acquista => {
+			// prepare data to upload
+			let fatture_to_upload = fattura_to_capture.upload_formatter_acquisti()?;
+			// upload to database
+			for fattura_to_upload in fatture_to_upload.into_iter() {
+				insert_insertable_struct_acquisti(fattura_to_upload, &mut conn)?;
+			}
+		},
+
+		InvoiceType::Vendita => {
+			// prepare data to upload
+			let fatture_to_upload = fattura_to_capture.upload_formatter_vendite()?;
+			// upload to database
+			for fattura_to_upload in fatture_to_upload.into_iter() {
+				insert_insertable_struct_vendite(fattura_to_upload, &mut conn)?
+			}
+		},
+	}
+
+	Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
