f15d766ab5ce1b88927db611377f883f74e2fcd2
DanielRivasMD <danielrivasmd@gmail.com>
2025-01-17 06:07:58 -0600

    add linux tools

---
+++ tools/package_release_linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e  # stop at first error
+
+[[ -z "$1" ]] && { echo "Please give package reference name as first parameter" ; exit 1; }
+FOLDER_NAME=fattura_${1}_Linux
+
+mkdir -p ${FOLDER_NAME}
+cp target/release/fattura ${FOLDER_NAME}
+cp LICENSE ${FOLDER_NAME}
+zip -r ${FOLDER_NAME}.zip ${FOLDER_NAME}
