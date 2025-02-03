dc8b5ae6bfbb8756d7312cac9308d3c28f66e878
Daniel Rivas <danielrivasmd@gmail.com>
2025-02-02 19:55:51 -0600

    format down sql

--- migrations/2025-01-06-134104_fatture/down.sql
+++ migrations/2025-01-06-134104_fatture/down.sql
@@ -1,6 +1,10 @@
+----------------------------------------------------------------------------------------------------
+
 DROP TABLE tabella_principale;
 DROP TABLE tabella_acquisti;
 DROP TABLE tabella_vendite;
 DROP TABLE tabella_sommario;
 DROP TABLE tabella_mensile;
 DROP TABLE tabella_annualmente;
+
+----------------------------------------------------------------------------------------------------
