ccaec73afd53ea6ecb8887cc1ac04c0bb5c4ee83
Daniel Rivas <danielrivasmd@gmail.com>
2025-06-06 13:05:20 +0200

    create invoice directories

--- install.sh
+++ install.sh
@@ -21,3 +21,19 @@ brew install sqlite
 brew install litecli
 
 ####################################################################################################
+
+# Verify if directories $HOME/Desktop/Acquista and $HOME/Desktop/Vendita exist, if not create them and log
+DIR1="$HOME/Desktop/Acquista"
+DIR2="$HOME/Desktop/Vendita"
+
+for DIR in "$DIR1" "$DIR2"; do
+    if [ ! -d "$DIR" ]; then
+        echo "Directory $DIR does not exist. Creating it..."
+        mkdir -p "$DIR"
+        echo "Directory $DIR created."
+    else
+        echo "Directory $DIR already exists."
+    fi
+done
+
+####################################################################################################
