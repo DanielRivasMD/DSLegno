b4d81f3248ed92b13afd0107a6678133fbf5cff4
Daniel Rivas <danielrivasmd@gmail.com>
2025-06-10 10:18:34 +0200

    patch watchers

--- fattura-watchers.sh
+++ fattura-watchers.sh
@@ -3,13 +3,13 @@
 
 # Launch watchexec for the Acquista folder (watching for XML changes)
 # This triggers the fattura executable with the "acquista" flag
-/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Acquista" -- sh -c 'for file in "$HOME/Desktop/Acquista"/*.xml; do "$HOME/bin/fattura" --input "$file" --invoice acquista; done' &
+/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Acquista" -- bash -c "for file in '$HOME/Desktop/Acquista'/*.xml; do '$HOME/bin/fattura' --input \"\$file\" --invoice acquista; done" &
 
 ####################################################################################################
 
 # Launch watchexec for the Vendita folder (watching for XML changes)
 # This triggers the fattura executable with the "vendita" flag
-/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Vendita" -- sh -c 'for file in "$HOME/Desktop/Vendita"/*.xml; do "$HOME/bin/fattura" --input "$file" --invoice vendita; done' &
+/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Vendita" -- bash -c "for file in '$HOME/Desktop/Vendita'/*.xml; do '$HOME/bin/fattura' --input \"$\file\" --invoice vendita; done" &
 
 ####################################################################################################
 
