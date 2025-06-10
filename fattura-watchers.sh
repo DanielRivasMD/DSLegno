4d647b57523cf181c25c85d75b742e51a1e76298
Daniel Rivas <danielrivasmd@gmail.com>
2025-06-10 10:13:25 +0200

    update watchers

--- fattura-watchers.sh
+++ fattura-watchers.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ####################################################################################################
 
-# Launch watchexec for the Vendita folder (watching for XML changes)
-# This triggers the fattura executable with the "vendita" flag
-/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Vendita" "$HOME/bin/fattura --input $HOME/Desktop/Vendita/*.xml --invoice vendita" &
+# Launch watchexec for the Acquista folder (watching for XML changes)
+# This triggers the fattura executable with the "acquista" flag
+/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Acquista" -- sh -c 'for file in "$HOME/Desktop/Acquista"/*.xml; do "$HOME/bin/fattura" --input "$file" --invoice acquista; done' &
 
 ####################################################################################################
 
-# Launch watchexec for the Acquista folder (watching for XML changes)
-# This triggers the fattura executable with the "acquista" flag
-/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Acquista" "$HOME/bin/fattura --input $HOME/Desktop/Acquista/*.xml --invoice acquista" &
+# Launch watchexec for the Vendita folder (watching for XML changes)
+# This triggers the fattura executable with the "vendita" flag
+/opt/homebrew/bin/watchexec -w "$HOME/Desktop/Vendita" -- sh -c 'for file in "$HOME/Desktop/Vendita"/*.xml; do "$HOME/bin/fattura" --input "$file" --invoice vendita; done' &
 
 ####################################################################################################
 
